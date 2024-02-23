9bf644c4fce5cb22ae1ddee6db7053a72d19f84d
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-02-23 23:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const age = form["age"].value;
+  const name = form["name"].value;
+
+  const prmosieCreator = new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`)
+        return;
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`)
+        return;
+      }
+    }, 4000)
+  })
+
+  prmosieCreator
+  .then((data) => {
+    alert(data)
+  })
+  .catch((err)=>{
+    alert(err)
+  })
+})
